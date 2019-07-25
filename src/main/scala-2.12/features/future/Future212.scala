5dd020b79d6668a88342ba3d9c82e1d6b29e5eea
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-25 19:17:11 +0200

    added Future.delegate example

--- src/main/scala-2.12/features/future/Future212.scala
+++ src/main/scala-2.12/features/future/Future212.scala
@@ -1,10 +1,39 @@
 package features.future
 
+import scala.concurrent._
+import scala.concurrent.duration._
+import features.chaining._
+
 object Future212 extends App {
 
     println("\n========== Future 2.12")
 
+    implicit lazy val ec: ExecutionContext = ExecutionContext.global
 
+    def squaredAsync(value: Int) = Future {
+        ">>> squaredAsync" tap println
+        value * value
+    }
+
+    def plus17(x: Int): Int = {
+        ">>> map" tap println
+        x + 17
+    }
+
+    "----- immediate execution:" tap println
+
+    val f1: Future[Int] = squaredAsync(5) map plus17
+
+    Await.result(f1, 3.seconds) tap println
+
+    "----- Future.apply + Future#flatten:" tap println
+
+    val f2: Future[Int] = Future.apply {
+        ">>> apply" tap println
+        squaredAsync(5)
+    }.flatten map plus17
+
+    Await.result(f2, 3.seconds) tap println
 
     println("==========\n")
 }
--- src/main/scala-2.13/features/future/Future213.scala
+++ src/main/scala-2.13/features/future/Future213.scala
@@ -1,10 +1,48 @@
 package features.future
 
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.util.chaining._
+
 object Future213 extends App {
 
     println("\n========== Future 2.13")
 
+    implicit lazy val ec: ExecutionContext = ExecutionContext.global
 
+    def squaredAsync(value: Int) = Future {
+        ">>> squaredAsync" tap println
+        value * value
+    }
+
+    def plus17(x: Int): Int = {
+        ">>> map" tap println
+        x + 17
+    }
+
+    "----- immediate execution:" tap println
+
+    val f1: Future[Int] = squaredAsync(5) map plus17
+
+    Await.result(f1, 3.seconds) tap println
+
+    "----- Future.apply + Future#flatten:" tap println
+
+    val f2: Future[Int] = Future.apply {
+        ">>> apply" tap println
+        squaredAsync(5)
+    }.flatten map plus17
+
+    Await.result(f2, 3.seconds) tap println
+
+    "----- Future.delegate:" tap println
+
+    val f3: Future[Int] = Future.delegate {
+        ">>> delegate" tap println
+        squaredAsync(5)
+    } map plus17
+
+    Await.result(f3, 3.seconds) tap println
 
     println("==========\n")
 }
