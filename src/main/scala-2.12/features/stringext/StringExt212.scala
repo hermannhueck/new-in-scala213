64e7a071214b99e05756352c22bec19fe7fd48d3
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-25 23:29:44 +0200

    added String#toXXXOption examples

---
+++ src/main/scala-2.12/features/stringext/StringExt212.scala
@@ -0,0 +1,33 @@
+package features.stringext
+
+import features.chaining._
+
+object StringExt212 extends App {
+
+    println("\n========== StringExt 2.12")
+
+    implicit class StringOps(private val s: String) {
+        import scala.util.Try
+        def toIntOption: Option[Int] = Try(s.toInt).toOption
+        def toDoubleOption: Option[Double] = Try(s.toDouble).toOption
+        def toBooleanOption: Option[Boolean] = Try(s.toBoolean).toOption
+    }
+
+    "----- String#toIntOption" tap println
+    val i1 = "42".toInt tap println
+    val iOpt1 = "42".toIntOption tap println
+    val iOpt2 = "x42".toIntOption tap println
+
+    "\n----- String#toDoubleOption" tap println
+    val d1 = "42.0".toDouble tap println
+    val dOpt1 = "42.0".toDoubleOption tap println
+    val dOpt2 = "42.x".toDoubleOption tap println
+
+    "\n----- String#toDoubleOption" tap println
+    val b1 = "true".toBoolean tap println
+    val bOpt1 = "true".toBooleanOption tap println
+    val bOpt2 = "false".toBooleanOption tap println
+    val bOpt3 = "no".toBooleanOption tap println
+
+    println("==========\n")
+}
