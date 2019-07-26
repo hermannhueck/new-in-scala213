944d2d45ffaadc0dd5bf0b68a90796e4ab06f880
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-26 19:16:27 +0200

    added examples for Collection#tapEach

---
+++ src/main/scala-2.12/feature/collections/tapeach/TapEach212.scala
@@ -0,0 +1,37 @@
+package feature.collections.tapeach
+
+import feature.chaining._
+
+import scala.collection.AbstractSeq
+
+import scala.language.higherKinds
+
+object TapEach212 extends App {
+
+  println("\n========== Coll#tapEach 2.12")
+
+  implicit class SeqOps[A](private val seq: Seq[A]) {
+    def tapEach(f: A => Unit): Seq[A] = seq.map { a => f(a); a }
+  }
+  implicit class ListOps[A](private val seq: List[A]) {
+    def tapEach(f: A => Unit): List[A] = seq.map { a => f(a); a }
+  }
+  implicit class VectorOps[A](private val seq: Vector[A]) {
+    def tapEach(f: A => Unit): Vector[A] = seq.map { a => f(a); a }
+  }
+  implicit class SetOps[A](private val seq: Set[A]) {
+    def tapEach(f: A => Unit): Set[A] = seq.map { a => f(a); a }
+  }
+  implicit class MapOps[K, V](private val seq: Map[K, V]) {
+    def tapEach(f: ((K, V)) => Unit): Map[K, V] = seq.map { pair => f(pair); pair }
+  }
+
+  val doubledAndSquared: List[Int] = List(1, 2, 3)
+    .tapEach(x => println(s"value: $x"))
+    .map(x => x * 2)
+    .tapEach(x => println(s"doubled: $x"))
+    .map(x => x * x)
+    .tapEach(x => println(s"squared: $x"))
+
+  println("==========\n")
+}
---
+++ src/main/scala-2.13/feature/collections/tapeach/TapEach213.scala
@@ -0,0 +1,17 @@
+package feature.collections.tapeach
+
+import scala.util.chaining._
+
+object TapEach213 extends App {
+
+  println("\n========== Coll#tapEach 2.13")
+
+  val doubledAndSquared = List(1, 2, 3)
+    .tapEach(x => println(s"value: $x"))
+    .map(x => x * 2)
+    .tapEach(x => println(s"doubled: $x"))
+    .map(x => x * x)
+    .tapEach(x => println(s"squared: $x"))
+
+  println("==========\n")
+}
