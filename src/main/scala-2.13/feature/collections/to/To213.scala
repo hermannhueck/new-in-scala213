7c31e87c5a5dc493d2ed40eb92dfa796273ba1fe
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-26 14:06:19 +0200

    added examples for Collection#to

---
+++ src/main/scala-2.13/feature/collections/to/To213.scala
@@ -0,0 +1,16 @@
+package feature.collections.to
+
+import scala.util.chaining._
+
+object To213 extends App {
+
+    println("\n========== Coll#to 2.13")
+
+    val map = Map("one" -> 1, "two" -> 2, "three" -> 3)
+
+    val l1 = map.toList tap println
+    // val l2 = map.to[List] tap println // not available in 2.13
+    val l3 = map.to(List) tap println
+
+    println("==========\n")
+}
