75f4d7e781e2555e9424bec332d0ba5bec985e96
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-27 08:22:48 +0200

    added LazyList examples

---
+++ src/main/scala-2.12/feature/collections/lazylist/LazyList212.scala
@@ -0,0 +1,20 @@
+package feature.collections.lazylist
+
+import feature.chaining._
+
+object LazyList212 extends App {
+
+  println("\n========== Stream/LazyList 2.12")
+
+  // Stream is lazy in it's tail, but eager in it's head
+
+  val stream: Stream[(Int, Int)] =
+    Stream.continually(42)
+      .take(10)
+      .zipWithIndex
+      .map { case (value, index) => (index, value) } tap println
+
+  stream.to[List] tap println
+
+  println("==========\n")
+}
---
+++ src/main/scala-2.13/feature/collections/lazylist/LazyList213.scala
@@ -0,0 +1,29 @@
+package feature.collections.lazylist
+
+import scala.util.chaining._
+
+object LazyList213 extends App {
+
+  println("\n========== Stream/LazyList 2.13")
+
+  // Stream is lazy in it's tail, but eager in it's head
+  // LazyList is lazy in it's head and tail
+
+  val stream: Stream[(Int, Int)] =
+    Stream.continually(42)
+      .take(10)
+      .zipWithIndex
+      .map { case (value, index) => (index, value) } tap println
+
+  stream.to(List) tap println
+
+  val ll: LazyList[(Int, Int)] =
+    LazyList.continually(42)
+      .take(10)
+      .zipWithIndex
+      .map { case (value, index) => (index, value) } tap println
+
+  ll.to(List) tap println
+
+  println("==========\n")
+}
