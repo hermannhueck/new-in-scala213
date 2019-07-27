ffe4d5f8667c5008e6113583104afdb0d8b3ab44
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-27 17:42:25 +0200

    added example for underscore in number literals

---
+++ src/main/scala-2.13/feature/language/UnderscoresInNumberLiterals213.scala
@@ -0,0 +1,19 @@
+package feature.language;
+
+import scala.util.chaining._
+
+object UnderscoresInNumberLiterals213 extends App {
+
+  println("\n========== Underscores in Number Literals 2.13")
+
+  val int0: Int = 1000000 tap println
+  val int1: Int = 1_000_000 tap println
+  val int2: Int = 1_0_0_0_0_0_0 tap println
+  // val int3: Int = 1_0_0_0_0_0_0_ tap println // compile error: trailing separator is not allowed
+
+  val long: Long = 1_000_000_000L tap println
+  val float: Float = 1_000.99f tap println
+  val double: Double = 1_000_000.999_999 tap println
+
+  println("==========\n")
+}
