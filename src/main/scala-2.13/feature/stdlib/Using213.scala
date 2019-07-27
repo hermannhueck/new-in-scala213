6e987efaba80555efa9e111f88d7f0791c274da7
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-27 17:43:23 +0200

    improved Using example

--- src/main/scala-2.13/feature/stdlib/Using213.scala
+++ src/main/scala-2.13/feature/stdlib/Using213.scala
@@ -15,20 +15,25 @@ object Using213 extends App {
     def readLines(reader: BufferedReader) =
         Iterator.unfold(())(_ => Option(reader.readLine()).map(_ -> ())).toList
 
-    val try1: Try[Seq[String]] =
-        Using(bufferedReader("README.md")) { reader => readLines(reader) }
+    def catFile(fileName: String): Unit = {
 
-    try1 match {
+      val `try`: Try[Seq[String]] =
+        Using(bufferedReader(fileName)) { reader => readLines(reader) }
+
+      `try` match {
         case Failure(exception) => exception.toString tap println
         case Success(lines) => lines foreach println
+      }
     }
+
+    catFile("README.md")
 
     "--------------------" tap println
 
-    val lines: Seq[String] =
-        Using.resource(bufferedReader("README.md")) { readLines }
+    def lines(fileName: String): Seq[String] =
+        Using.resource(bufferedReader(fileName)) { readLines }
 
-    lines foreach println
+    lines("README.md") foreach println
 
     "--------------------" tap println
 
@@ -39,7 +44,7 @@ object Using213 extends App {
 
     val List(r1, r2, r3) = List("1st", "2nd", "3rd").map(Resource)
 
-    val try2: Try[Seq[String]] = for {
+    val `try`: Try[Seq[String]] = for {
         lines1 <- Using(r1)(_.lines)
         lines2 <- Using(r2)(_.lines)
         lines3 <- Using(r3)(_.lines)
@@ -47,7 +52,7 @@ object Using213 extends App {
         lines1 ++ lines2 ++ lines3
     }
 
-    try2 foreach { _ foreach println }
+    `try` foreach { _ foreach println }
 
   println("==========\n")
 }
