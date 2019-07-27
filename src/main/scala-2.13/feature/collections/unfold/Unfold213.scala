44fcde569b240f6fa37f95d0786c717bff7dd205
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-27 08:02:11 +0200

    added Coll#lengthIs examples

---
+++ src/main/scala-2.13/feature/collections/unfold/Unfold213.scala
@@ -0,0 +1,34 @@
+package feature.collections.unfold
+
+import scala.util.chaining._
+import scala.util.Using
+
+import java.io.{BufferedReader, FileReader}
+
+object Unfold213 extends App {
+
+  println("\n========== Coll.unfold 2.13")
+
+  def bufferedReader(fileName: String) =
+    new BufferedReader(new FileReader(fileName))
+
+  def readLines(reader: BufferedReader) =
+    Iterator.unfold(())(_ => Option(reader.readLine()).map(_ -> ())).toList
+
+  def readLines2(reader: BufferedReader): List[String] = {
+    val initialState: Unit = ()
+    val iterator: Iterator[String] = Iterator.unfold(initialState) { _ =>
+      val maybeLine: Option[String] = Option(reader.readLine())
+      val maybeLineState: Option[(String, Unit)] = maybeLine.map(_ -> ())
+      maybeLineState
+    }
+    iterator.toList
+  }
+
+  val lines: Seq[String] =
+    Using.resource(bufferedReader("README.md"))(readLines2)
+
+  lines foreach println
+
+  println("==========\n")
+}
