cf166ea8a0994b85350c55758e7165edd15e65a2
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-28 07:53:10 +0200

    commented examples: MapView

--- src/main/scala-2.12/feature/collections/MapView212.scala
+++ src/main/scala-2.12/feature/collections/MapView212.scala
@@ -9,10 +9,14 @@ object MapView212 extends App {
     val kvs: Map[String, Int] = Map("one" -> 1, "two" -> 2, "three" -> 3) tap println
     val kvsFlipped: Map[Int, String] = kvs.toList.map { case (fst, snd) => (snd, fst) }.toMap tap println
 
-    ">>> mapValues:" tap println
+    "\n>>> Map#mapValues returns Map in 2.12:" tap println
+    // def mapValues[W](f: (V) ⇒ W): Map[K, W]
+    // Map#mapValues returns Map in 2.12
     val mappedValues: Map[String, Int] = kvs.mapValues(_ + 10) tap println
 
-    ">>> filterKeys:" tap println
+    "\n>>> Map#filterKeys returns Map in 2.12:" tap println
+    // def filterKeys(p: (K) ⇒ Boolean): Map[K, V]
+    // Map#filterKeys returns Map in 2.12
     val keysFiltered: Map[Int, String] = kvsFlipped.filterKeys(_ %2 != 0) tap println
 
     println("==========\n")
