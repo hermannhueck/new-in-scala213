3621f9dd5b73cfeee0585a8c325d67f6af4adde6
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-29 08:50:39 +0200

    added example: By Name Implicits

---
+++ src/main/scala-2.13/feature/language/ByNameImplicits213.scala
@@ -0,0 +1,28 @@
+package feature.language
+
+import scala.util.chaining._
+
+/*
+  see:
+  https://docs.scala-lang.org/sips/byname-implicits.html
+ */
+object ByNameImplicits213 extends App {
+
+  println("\n========== By Name Implicit Parameters 2.13")
+
+  trait Foo {
+    def next: Foo
+  }
+
+  object Foo {
+    // wouldn't compile, if rec were a by value parameter
+    // remove the => and try to compile ...
+    implicit def foo(implicit rec: => Foo): Foo =
+      new Foo { def next = rec }
+  }
+
+  val foo = implicitly[Foo]
+  assert(foo eq foo.next)
+
+  println("==========\n")
+}
