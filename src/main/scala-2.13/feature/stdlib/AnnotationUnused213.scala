c4291e66251ed9e813453c245013752537a17685
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-29 09:10:33 +0200

    added example: scala.annotation.unused

---
+++ src/main/scala-2.13/feature/stdlib/AnnotationUnused213.scala
@@ -0,0 +1,17 @@
+package feature.stdlib
+
+import scala.annotation.unused
+
+object AnnotationUnused213 extends App {
+
+    println("\n========== Annotation @unused 2.13")
+
+    // Equivalent of SuppressWarnings("unused")
+    @unused         // enable -Xlint
+    val x = 5
+
+    val y = 6
+    println(y)
+
+    println("==========\n")
+}
