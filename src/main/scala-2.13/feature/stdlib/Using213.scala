73570c833e6f2701d37e374a3d127e0ca5c68163
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-29 06:24:04 +0200

    code formatted

--- src/main/scala-2.13/feature/stdlib/Using213.scala
+++ src/main/scala-2.13/feature/stdlib/Using213.scala
@@ -1,4 +1,6 @@
-package feature.stdlib;
+package feature.stdlib
+
+;
 
 import java.io.{BufferedReader, FileReader}
 
@@ -9,50 +11,55 @@ object Using213 extends App {
 
   println("\n========== Using 2.13")
 
-    def bufferedReader(fileName: String) =
-        new BufferedReader(new FileReader(fileName))
+  def bufferedReader(fileName: String) =
+    new BufferedReader(new FileReader(fileName))
 
-    def readLines(reader: BufferedReader) =
-        Iterator.unfold(())(_ => Option(reader.readLine()).map(_ -> ())).toList
+  def readLines(reader: BufferedReader) =
+    Iterator.unfold(())(_ => Option(reader.readLine()).map(_ -> ())).toList
 
-    def catFile(fileName: String): Unit = {
+  def catFile(fileName: String): Unit = {
 
-      val `try`: Try[Seq[String]] =
-        Using(bufferedReader(fileName)) { reader => readLines(reader) }
+    val `try`: Try[Seq[String]] =
+      Using(bufferedReader(fileName)) { reader => readLines(reader) }
 
-      `try` match {
-        case Failure(exception) => exception.toString tap println
-        case Success(lines) => lines foreach println
-      }
+    `try` match {
+      case Failure(exception) => exception.toString tap println
+      case Success(lines) => lines foreach println
+    }
+  }
+
+  catFile("README.md")
+
+  "--------------------" tap println
+
+  def lines(fileName: String): Seq[String] =
+    Using.resource(bufferedReader(fileName)) {
+      readLines
     }
 
-    catFile("README.md")
+  lines("README.md") foreach println
 
-    "--------------------" tap println
+  "--------------------" tap println
 
-    def lines(fileName: String): Seq[String] =
-        Using.resource(bufferedReader(fileName)) { readLines }
+  final case class Resource(name: String) extends AutoCloseable {
+    override def close(): Unit = println(s"Closing $name")
 
-    lines("README.md") foreach println
+    def lines: List[String] = List(s"$name: line 1", s"$name: line 2")
+  }
 
-    "--------------------" tap println
+  val List(r1, r2, r3) = List("1st", "2nd", "3rd").map(Resource)
 
-    final case class Resource(name: String) extends AutoCloseable {
-        override def close(): Unit = println(s"Closing $name")
-        def lines: List[String] = List(s"$name: line 1", s"$name: line 2")
-    }
+  val `try`: Try[Seq[String]] = for {
+    lines1 <- Using(r1)(_.lines)
+    lines2 <- Using(r2)(_.lines)
+    lines3 <- Using(r3)(_.lines)
+  } yield {
+    lines1 ++ lines2 ++ lines3
+  }
 
-    val List(r1, r2, r3) = List("1st", "2nd", "3rd").map(Resource)
-
-    val `try`: Try[Seq[String]] = for {
-        lines1 <- Using(r1)(_.lines)
-        lines2 <- Using(r2)(_.lines)
-        lines3 <- Using(r3)(_.lines)
-    } yield {
-        lines1 ++ lines2 ++ lines3
-    }
-
-    `try` foreach { _ foreach println }
+  `try` foreach {
+    _ foreach println
+  }
 
   println("==========\n")
 }
