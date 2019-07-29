1ae09d012e8408d25bfd59db329983134691d909
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-29 06:22:53 +0200

    added example: s-Interpolator in Pattern Matches

---
+++ src/main/scala-2.13/feature/language/SInterpolatorInPatternMatches213.scala
@@ -0,0 +1,18 @@
+package feature.language;
+
+import scala.util.chaining._
+
+object SInterpolatorInPatternMatches213 extends App {
+
+  println("\n========== s-Interpolator in Pattern Matches 2.13")
+
+  val dateString = "11-June-2019" tap println
+
+  val s"$day-$month-$year" = dateString
+
+  year tap println
+  month tap println
+  day tap println
+
+  println("==========\n")
+}
