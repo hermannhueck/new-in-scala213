6718345fffe7fa6572ea58c1d00d53890d9c0aa7
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-30 16:33:07 +0200

    StringOps refactored

--- src/main/scala-2.12/compat213/string/package.scala
+++ src/main/scala-2.12/compat213/string/package.scala
@@ -1,9 +1,10 @@
 package compat213
 
+import scala.util.Try
+
 package object string {
 
   implicit class StringOps(private val s: String) {
-    import scala.util.Try
     def toIntOption: Option[Int] = Try(s.toInt).toOption
     def toDoubleOption: Option[Double] = Try(s.toDouble).toOption
     def toBooleanOption: Option[Boolean] = Try(s.toBoolean).toOption
