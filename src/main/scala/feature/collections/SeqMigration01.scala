9b2347f7512047993563017d16b5130abcc4c525
Hermann Hueck <hermann.hueck@mailbox.org>
2019-07-31 15:01:24 +0200

    added example: Seq Migration

---
+++ src/main/scala/feature/collections/SeqMigration01.scala
@@ -0,0 +1,27 @@
+package feature.collections
+
+import scala.collection.compat.immutable.ArraySeq
+import scala.collection.mutable.ArrayBuffer
+
+object SeqMigration01 extends App {
+
+    println("\n========== SeqMigration Strategy 01")
+    trait Order
+    trait Food
+
+    def orderFood(order: collection.Seq[Order]): collection.Seq[Food] = {
+        Seq(new Food{})
+    }
+
+    val orderArray = Array(new Order {})
+
+    val food2 = orderFood(orderArray)
+
+    val food3 = orderFood(orderArray.toSeq)
+    val food3b = orderFood(ArrayBuffer(new Order{}).toSeq)
+    val food4 = orderFood(orderArray.toIndexedSeq)
+
+    val food5 = orderFood(ArraySeq.unsafeWrapArray(Array(new Order{})))
+
+    println("==========\n")
+}
---
+++ src/main/scala/feature/collections/SeqMigration03.scala
@@ -0,0 +1,28 @@
+package feature.collections
+
+import scala.collection.compat.immutable.ArraySeq
+import scala.collection.immutable
+import scala.collection.mutable.ArrayBuffer
+
+object SeqMigration03 extends App {
+
+    println("\n========== SeqMigration Strategy 03")
+    trait Order
+    trait Food
+
+    def orderFood(order: immutable.Seq[Order]): immutable.Seq[Food] = {
+        immutable.Seq(new Food{})
+    }
+
+    val orderArray = Array(new Order {})
+
+    val food2 = orderFood(orderArray.toIndexedSeq) // or use .toList
+
+    val food3 = orderFood(orderArray.toIndexedSeq) // or use .toList
+    val food3b = orderFood(ArrayBuffer(new Order{}).toIndexedSeq) // or use .toList
+    val food4 = orderFood(orderArray.toIndexedSeq)
+
+    val food5 = orderFood(ArraySeq.unsafeWrapArray(Array(new Order{})))
+
+    println("==========\n")
+}
