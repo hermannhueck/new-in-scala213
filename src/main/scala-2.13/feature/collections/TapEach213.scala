a5571c977000797010c4808bed39954165f062f7
Hermann Hueck <hermann.hueck@mailbox.org>
2019-08-01 13:35:22 +0200

    formatted example tapEach

--- src/main/scala-2.13/feature/collections/TapEach213.scala
+++ src/main/scala-2.13/feature/collections/TapEach213.scala
@@ -4,12 +4,13 @@ object TapEach213 extends App {
 
   println("\n========== Coll#tapEach 2.13")
 
-  val doubledAndSquared = List(1, 2, 3)
-    .tapEach(x => println(s"value: $x"))
-    .map(x => x * 2)
-    .tapEach(x => println(s"doubled: $x"))
-    .map(x => x * x)
-    .tapEach(x => println(s"squared: $x"))
+  val doubledAndSquared =
+    List(1, 2, 3)
+      .tapEach(x => println(s"value: $x"))
+      .map(x => x * 2)
+      .tapEach(x => println(s"doubled: $x"))
+      .map(x => x * x)
+      .tapEach(x => println(s"squared: $x"))
 
   println("==========\n")
 }
