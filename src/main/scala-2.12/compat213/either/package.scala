4e0f9df3a51ac5237c21eb9fe9ded2b64e17a685
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-12 00:25:41 +0200

    type params for EitherOps made covariant

--- src/main/scala-2.12/compat213/either/package.scala
+++ src/main/scala-2.12/compat213/either/package.scala
@@ -2,13 +2,13 @@ package compat213
 
 package object either {
 
-  implicit class RightOps[L, R](private val right: Right[L, R]) extends AnyVal {
+  implicit class RightOps[+L, +R](private val right: Right[L, R]) extends AnyVal {
 
     @inline def withLeft[LL](implicit ev: L <:< LL): Either[LL, R] =
       right.asInstanceOf[Either[LL, R]]
   }
 
-  implicit class LeftOps[L, R](private val left: Left[L, R]) extends AnyVal {
+  implicit class LeftOps[+L, +R](private val left: Left[L, R]) extends AnyVal {
 
     @inline def withRight[RR](implicit ev: R <:< RR): Either[L, RR] =
       left.asInstanceOf[Either[L, RR]]
