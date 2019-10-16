6a4d431a0d871a46b924aebca8228a4bf2b664d3
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-17 00:44:01 +0200

    added example ProductEnhancer for 2.13

---
+++ src/main/scala-2.13/productenhancer/ProductEnhancer.scala
@@ -0,0 +1,17 @@
+package productenhancer
+
+object ProductEnhancer {
+
+  implicit class Ops(product: Product) {
+
+    import product._
+
+    val names  = productElementNames.toList
+    val values = (0 until productArity).toList map productElement
+    val pairs  = names zip values
+
+    @inline def toJson: Json = Json.pairsToJson(pairs)
+
+    @inline def toJsonString: String = toJson.jsonString
+  }
+}
