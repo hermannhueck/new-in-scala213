673a74dd1d86b3bea7fefffe7d286d1e625ee803
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-17 20:12:05 +0200

    unifed SymbolLiterals212 and SymbolLiterals213 into one file

--- src/main/scala/feature/language/SymbolLiterals212and213.scala
+++ src/main/scala/feature/language/SymbolLiterals212and213.scala
@@ -4,13 +4,13 @@ import scala.util.chaining._
 
 import util.formatting._
 
-object SymbolLiterals213 extends App {
+object SymbolLiterals212and213 extends App {
 
   prtTitleObjectName(this)
 
   prtSubTitle("Symbol Literals deprecated in 2.13")
 
-  val symbol1 = 'symbol1 // deprecated
+  val symbol1 = 'symbol1 // deprecated in 2.13
   symbol1 tap println
 
   val symbol2 = Symbol("symbol2") // new: create symbol with Symbol.apply
