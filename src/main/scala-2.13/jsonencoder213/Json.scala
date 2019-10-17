070f84b0e98b2d05cfd39387d86f6a03bcf2550b
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-17 19:30:05 +0200

    removed example jsonencoder213

--- src/main/scala-2.13/jsonencoder213/Json.scala
+++
@@ -1,9 +0,0 @@
-package jsonencoder213
-
-sealed trait Json
-case class JsonObject(fields: List[(String, Json)]) extends Json
-case class JsonArray(items: List[Json])             extends Json
-case class JsonString(value: String)                extends Json
-case class JsonNumber(value: Double)                extends Json
-case class JsonBoolean(value: Boolean)              extends Json
-case object JsonNull                                extends Json
--- src/main/scala-2.13/jsonencoder213/Person.scala
+++
@@ -1,9 +0,0 @@
-package jsonencoder213
-
-sealed trait Gender extends Product with Serializable
-case object Male    extends Gender
-case object Female  extends Gender
-
-case class Person(name: String, age: Int, isFemale: Boolean, email: String) {
-  def tupled: (String, Int, Boolean, String) = Person.unapply(this).get
-}
