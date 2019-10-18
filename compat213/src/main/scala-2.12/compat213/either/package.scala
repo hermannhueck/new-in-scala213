5422716f62d3fac4f9b108c7b2566b6db8c4148c
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-18 13:19:40 +0200

    EitherOps improved in compat213.either

--- compat213/src/main/scala-2.12/compat213/either/package.scala
+++ compat213/src/main/scala-2.12/compat213/either/package.scala
@@ -3,31 +3,18 @@ package compat213
 package object either {
 
   implicit class RightOps[+L, +R](private val right: Right[L, R]) extends AnyVal {
-
-    @inline def withLeft[LL](implicit ev: L <:< LL): Either[LL, R] =
-      right.asInstanceOf[Either[LL, R]]
+    @inline def withLeft[LL >: L]: Either[LL, R] = right
   }
 
   implicit class LeftOps[+L, +R](private val left: Left[L, R]) extends AnyVal {
-
-    @inline def withRight[RR](implicit ev: R <:< RR): Either[L, RR] =
-      left.asInstanceOf[Either[L, RR]]
+    @inline def withRight[RR >: R]: Either[L, RR] = left
   }
 
   implicit class EitherOps[+L, +R](private val either: Either[L, R]) extends AnyVal {
 
-    @inline def flatten[L1 >: L, RR](implicit ev: R <:< Either[L1, RR]): Either[L1, RR] =
+    @inline def flatten[L1 >: L, RR](
+        implicit ev: R <:< Either[L1, RR]
+    ): Either[L1, RR] =
       either.flatMap(x => x)
   }
-
-  /* my 1st working impl
-
-  implicit class LeftFlattenOps[L, R](private val left: Left[L, R]) extends AnyVal {
-    @inline def flatten: Either[L, R] = left
-  }
-
-  implicit class RightFlattenOps[LL, L, R](private val right: Right[LL, Either[L, R]]) extends AnyVal {
-    @inline def flatten: Either[L, R] = right.toOption.get
-  }
- */
 }
