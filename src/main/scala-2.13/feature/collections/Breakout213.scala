cbc6d53e8ce6ad6eb945164a67fb0f4b6a3335d1
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-18 19:48:22 +0200

    improved breakOut replacement example in  2.13

--- src/main/scala-2.13/feature/collections/Breakout213.scala
+++ src/main/scala-2.13/feature/collections/Breakout213.scala
@@ -17,18 +17,19 @@ object Breakout213 extends App {
   val toPair: Int => (Int, Int) = x => x -> x
   println
 
-  // In 2.13 we use the to method to convert the mapped iterator into the target collection.
+  // In 2.13 we use the to method to convert the mapped view into the target collection.
+  // The lazy Views are not necessary in this case, but recommended for performance reasons.
 
-  val list2                      = list.iterator.map(toPair)
-  val array: Array[(Int, Int)]   = list.iterator.map(toPair).to(Array)
-  val stream: Stream[(Int, Int)] = list.iterator.map(toPair).to(Stream)
-  val seq: Seq[(Int, Int)]       = list.iterator.map(toPair).to(Seq)
-  val set: Set[(Int, Int)]       = list.iterator.map(toPair).to(Set)
-  val map: Map[Int, Int]         = list.iterator.map(toPair).to(Map)
+  val list2                        = list.view.map(toPair).to(List)
+  val array: Array[(Int, Int)]     = list.view.map(toPair).to(Array)
+  val stream: LazyList[(Int, Int)] = list.view.map(toPair).to(LazyList)
+  val seq: Seq[(Int, Int)]         = list.view.map(toPair).to(Seq)
+  val set: Set[(Int, Int)]         = list.view.map(toPair).to(Set)
+  val map: Map[Int, Int]           = list.view.map(toPair).to(Map)
 
   ">>> target type: List" tap println; println(list2)
   ">>> target type: Array[Int, Int]" tap println; println(array)
-  ">>> target type: Stream[Int, Int]" tap println; println(stream)
+  ">>> target type: LazyList[Int, Int]" tap println; println(stream)
   ">>> target type: Seq[Int, Int]" tap println; println(seq)
   ">>> target type: Set[Int, Int]" tap println; println(set)
   ">>> target type: Map[Int, Int]" tap println; println(map)
