3e406f40871ffb5b62f117cb47db464286f225d8
Hermann Hueck <hermann.hueck@mailbox.org>
2019-10-19 18:08:38 +0200

    deprecated Stream code commented out in example LazyList213

--- src/main/scala-2.13/feature/collections/LazyList213.scala
+++ src/main/scala-2.13/feature/collections/LazyList213.scala
@@ -13,14 +13,17 @@ object LazyList213 extends App {
   // Stream is lazy in it's tail, but eager in it's head
   // LazyList is lazy in it's head and tail
 
-  val stream: Stream[(Int, Int)] = // Stream deprecated in 2.13
-    Stream
-      .continually(42)
-      .take(10)
-      .zipWithIndex
-      .map { case value -> index => index -> value } tap println
+  // Stream is deprecated in 2.13
+  /*
+    val stream: Stream[(Int, Int)] =
+      Stream
+        .continually(42)
+        .take(10)
+        .zipWithIndex
+        .map { case value -> index => index -> value } tap println
 
-  stream.to(List) tap println
+    stream.to(List) tap println
+   */
 
   val ll: LazyList[(Int, Int)] =
     LazyList
